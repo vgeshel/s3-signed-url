dea780abc550f882e9aa47c4899a377a0b79d062
Vadim Geshel <vadim@yummly.com>
2017-09-29 16:30:20 -0700

    Use official AWS SDK

--- s3-signed-url.go
+++ s3-signed-url.go
@@ -1,26 +1,23 @@
 package main
 
 import (
-	"github.com/mitchellh/goamz/aws"
-	"github.com/mitchellh/goamz/s3"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"fmt"
 	"flag"
 	"time"
 	"os"
 	"io"
-	"net/http"
-	"crypto/tls"
 )
 
 func main() {
 	bucket := flag.String("bucket", "", "bucket")
 	key := flag.String("key", "", "key")
-	region := flag.String("region", aws.USEast.Name, "region")
-	expiration := flag.String("expiration", "1h", "expiration in the Go duration format.  A duration string is a possibly signed sequence of decimal numbers, each with optional fraction and a unit suffix, such as \"300ms\", \"-1.5h\" or \"2h45m\". Valid time units are \"ns\", \"us\" (or \"µs\"), \"ms\", \"s\", \"m\", \"h\".")
-	verbose := flag.Bool("v", false, "verbose")
+	region := flag.String("region", "us-east-1", "region")
 	timing := flag.Bool("t", false, "show timing")
 	mode := flag.String("mode", "sign", "sign or cat")
-	insecure := flag.Bool("insecure", false, "turn on InsecureSkipVerify: http://golang.org/pkg/crypto/tls/#Config")
 
 	flag.Parse()
 
@@ -30,53 +27,36 @@ func main() {
 		os.Exit(1)
 	}
 
-	auth, err := aws.GetAuth("", "")
-
-	if err != nil {
-		panic(err)
+	s3c := s3.New(session.New(&aws.Config{
+		Region: aws.String(*region),
+	}))
+	input := &s3.GetObjectInput{
+		Bucket: aws.String(*bucket),
+		Key:    aws.String(*key),
 	}
-
-	if *verbose {
-		fmt.Fprintf(os.Stderr, "auth: %#v\n", auth)
-	}
-
-	reg, found := aws.Regions[*region]
-
-	if ! found {
-		panic(fmt.Sprintf("invalid region: %s", *region))
-	}
-
-	s3c := s3.New(auth, reg)
-	buck := s3c.Bucket(*bucket)
 
 	switch *mode {
-	case "sign":
-		dur, err := time.ParseDuration(*expiration)
-
-		if err != nil {
-			panic(err)
-		}
-
-		exp := time.Now().Add(dur)
-
-		fmt.Println(buck.SignedURL(*key, exp))
-
 	case "cat":
-		if *insecure {
-			tr := &http.Transport{
-				TLSClientConfig:    &tls.Config{InsecureSkipVerify: true},
-			}
-			http.DefaultClient.Transport = tr
-		}
-
-		reader, err := buck.GetReader(*key)
+		result, err := s3c.GetObject(input)
 
 		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+				case s3.ErrCodeNoSuchKey:
+					fmt.Println(s3.ErrCodeNoSuchKey, aerr.Error())
+				default:
+					fmt.Println(aerr.Error())
+				}
+			} else {
+				// Print the error, cast err to awserr.Error to get the Code and
+				// Message from an error.
+				fmt.Println(err.Error())
+			}
 			panic(err)
 		}
 
 		before := time.Now()
-		io.Copy(os.Stdout, reader)
+		io.Copy(os.Stdout, result.Body)
 
 		if *timing {
 			fmt.Fprintf(os.Stderr, "done %s/%s in %v\n", *bucket, *key, time.Now().Sub(before))
